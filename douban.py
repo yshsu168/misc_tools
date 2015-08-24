d935fba0550ed13bf4688e7c2eec54f922692c93
yshsu168 <yshsu168@gmail.com>
2015-08-24 16:53:28 +0800

    python script to play doubanFM

---
+++ douban.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+# coding: utf-8
+# Python program to dump the playlist from doubanfm.
+# And play it with mpg321
+# The channel id can be obtabined by http://douban.fm/j/explore/channel_detail?channel_id=101
+# 1: 华语, 2: 欧美, 3: 七零, 4: 八零, 5: 九零, 6: 粤语, 7: 摇滚, 8: 民谣, 9: 轻音乐, 10: 电影原声
+# 2015 @Samue Hsu
+
+import httplib
+import json
+import os
+import sys
+import subprocess
+import time
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+while True:
+# if True:
+    # connect to doubanfm web server
+    httpConnection = httplib.HTTPConnection('douban.fm')
+    httpConnection.request('GET', '/j/mine/playlist?type=n&channel=10')
+    song = json.loads(httpConnection.getresponse().read())['song']
+
+    # dump the required field and play it with mpg321
+    for curr_song in song:
+       print curr_song['title'] + ":" +  str(curr_song['length']) + " seconds"
+       # print curr_song['url']
+       mpd_cmd = subprocess.Popen(['mpg321', curr_song['url']])
+       mpd_cmd.wait()
+       # time.sleep(curr_song['length'])
+
+    '''
+    picture = 'images/' + song[0]['picture'].split('/')[4]
+
+    # 下载专辑封面
+    if not os.path.exists(picture):
+        subprocess.call([
+            'wget',
+            '-P',
+            'images',
+            song[0]['picture']])
+
+    # 发送桌面通知
+    subprocess.call([
+        'notify-send',
+        '-i',
+        os.getcwd() + '/' + picture,
+        song[0]['title'],
+        song[0]['artist'] + '\n' + song[0]['albumtitle']])
+    '''
